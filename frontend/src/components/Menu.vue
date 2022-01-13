d7911ca6cf6cffdd5027264c2b2a7810f4437951
ZeZeZe99 <rhuang@oberlin.edu>
2022-01-13 00:39:37 -0500

    Menu

---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+
+<!--    Toolbar at the top-->
+    <v-toolbar>
+
+<!--      Title-->
+      <v-toolbar-title>Menu of {{restaurant.name}}</v-toolbar-title>
+    </v-toolbar>
+
+<!--    Tabs to navigate through all bars of the restaurant-->
+    <v-tabs vertical>
+      <v-tab v-for="item in restaurant.bars" :key="item.id">
+        {{item.name}}
+      </v-tab>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+
+  props:{
+    restaurant: {required: true, type: Object}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
