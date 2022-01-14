5256cec62c6231ba0fa330f792a50b1197634ee2
liuyuda <yliu9@oberlin.edu>
2022-01-14 00:24:14 -0500

    food search template added

--- frontend/src/components/FoodSearch.vue
+++ frontend/src/components/FoodSearch.vue
@@ -1,10 +1,34 @@
 <template>
-  <div> 23333 </div>
+  <div>
+    <h2>To search, or not to search, that is the question,</h2>
+    <v-card>
+      <v-container fluid>
+        <v-row
+            align="center"
+        >
+          <v-col cols="12">
+            <v-autocomplete
+                v-model="value"
+                :items="items"
+                dense
+                filled
+                label="Filled"
+            ></v-autocomplete>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "FoodSearch"
+  name: "FoodSearch",
+  data: () => ({
+    items: ['foo', 'bar', 'fizz', 'buzz'],
+    values: ['foo', 'bar'],
+    value: null,
+  }),
 }
 </script>
 
