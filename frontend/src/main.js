95f1814d2f1932da28d30283919c46a53334235f
quta233 <liurantina@outlook.com>
2022-01-21 15:36:02 -0500

    add login/register frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from "@/plugins/vuetify";
 
-
+// var axios = require('axios')
+// //giaxios.defaults.baseURL = 'http://localhost:8080/api'
+//
+// Vue.prototype.$axios = axios
 // Vue.config.productionTip = false
 
 new Vue({
