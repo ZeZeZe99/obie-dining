4e2c0ac512e9bcd1d5722c022fef7b19dada66fb
liuyuda <yliu9@oberlin.edu>
2022-01-22 18:48:26 -0500

    Contact and About added

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card flat>
-
+    <br><br><br><br><br><br><br><br><br><br>
     <v-form v-model="valid">
       <v-container>
         <v-row align="center">
@@ -17,18 +17,18 @@
           </v-col>
           <v-col
               cols="12"
-              md= "4">
-            <v-btn>
-              Send! <small>(Under Construction)</small>
-            </v-btn>
-          </v-col>
-          <v-col
-              cols="12"
               md="4"
           >
             <v-text-field
                 label="Your Comments"
             ></v-text-field>
+          </v-col>
+          <v-col
+              cols="12"
+              md= "4">
+            <v-btn>
+              Send! <small>(Under Construction)</small>
+            </v-btn>
           </v-col>
         </v-row>
       </v-container>
